c3c9b78adb9a378ce223c2bc02c0cdf9dce88f91
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-15 09:31:53 -0700

    Added forgotten <ul> for listed attractions. Added .js DOM elements.

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 /* Imports */
 
 /* Get DOM Elements */
+const cityInput = document.getElementById('city-input');
+const climateSelect = document.getElementById('climate-select');
+const architectureSelect = document.getElementById('architecture-select');
+const attractionsInput = document.getElementById('attractions-input');
+const attractionsButton = document.getElementById('attractions-button');
+
+const outputSection = document.getElementById('output-section');
+const cityName = document.getElementById('city-name');
+const climateImg = document.getElementById('climate-img');
+const architectureImg = document.getElementById('architectue-img');
+const listAttractions = document.getElementById('list-attractions');
 
 /* State */
 
