7b71cb5324f7ce94b45af2401a85089a5fd1ebcf
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-15 10:54:30 -0700

    changed some minor HTML, added minor CSS changes, added first .js functions

--- app.js
+++ app.js
@@ -14,7 +14,54 @@ const architectureImg = document.getElementById('architectue-img');
 const listAttractions = document.getElementById('list-attractions');
 
 /* State */
+const city = {
+    name: 'Beardtopia',
+    climate: 'Tropical',
+    architecture: 'Beaux-Arts',
+    attractions: [],
+};
 
+cityInput.addEventListener('input', () => {
+    city.name = cityInput.value;
+    displayCity();
+});
+
+climateSelect.addEventListener('change', () => {
+    city.climate = climateSelect.value;
+    displayCity();
+});
+
+architectureSelect = addEventListener('change', () => {
+    city.architecture = architectureSelect.value;
+    displayCity();
+});
+
+attractionsInput.addEventListener('click', () => {
+    const attractions = attractionsInput.value;
+    city.attractions.push(attractions);
+    displayAttractions();
+    attractionsInput.value = '';
+});
+
+attractionsButton.addEventListener('click', () => {
+    const attractions = attractionsInput.value;
+    city.attractions.push(attractions);
+    displayAttractions();
+    attractionsInput.value = '';
+});
+
+function displayControls() {
+    cityInput.value = city.name;
+    climateSelect.value = city.climate;
+    architectureSelect.value = city.architecture;
+}
+
+function displayCity() {
+    outputSection.classList.value = '';
+    outputSection.classList.add(city.climate, city.architecture);
+    cityName.textContent = city.name;
+    climateImg.src = 'assets/';
+}
 /* Events */
 
 /* Display Functions */
